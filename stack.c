0a679eaa987929b6903e1f01483576bfb6ce3d7c
Nguyen Xuan Phuoc <phuoc.nx205232@sis.hust.edu.vn>
2022-12-07 15:57:20 +0700

    Completed stack.c

--- stack.c
+++ stack.c
@@ -1,10 +1,18 @@
+/**
+ * @file stack.c
+ * @author Nguyễn Xuân Phước - 20205232 (phuoc.nx205232@sis.hust.edu.vn)
+ * @brief File chứa các hàm của stack
+ * @date 2022-12-07
+ * 
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include "stack.h"
 
 
 void create(){
-    pTop = NULL;
+    pTop = NULL;    // Khởi tạo stack rỗng
 }
 
 /**
@@ -15,15 +23,15 @@ void create(){
  * 
  * @param data Giá trị của node mới
  */
-void push(int data){
-    struct stackNode *temp = (struct stackNode*)malloc(sizeof(struct stackNode));
-    if(!temp){
-        printf("\nHeap Overflow");
+void push(int data){    // Thêm một phần tử vào stack
+    struct stackNode *temp = (struct stackNode*)malloc(sizeof(struct stackNode)); // 
+    if(!temp){          // Nếu không còn bộ nhớ
+        printf("\nHeap Overflow"); 
         exit(1);
     }
-    temp -> data = data;
-    temp -> prev = pTop;
-    pTop = temp;
+    temp -> data = data;    // Gán giá trị cho node mới
+    temp -> prev = pTop;    // Node mới trỏ đến top
+    pTop = temp;            // Cập nhật lại top
 }
 
 /**
@@ -34,18 +42,18 @@ void push(int data){
  * 
  * @return Phần tử đầu tiên trong stack
  */
-void pop(){
+void pop(){         // Lấy một phần tử ra khỏi stack
     struct stackNode *temp;
 
-    if(pTop == NULL){
-        printf("\nStack Underflow");
-        exit(1);
+    if(pTop == NULL){   // Nếu stack rỗng
+        printf("\nStack Underflow");    // In ra thông báo
+        exit(1);        
     }
-    else{
-        temp = pTop;
-        pTop = pTop->prev;
-        temp->prev = NULL;
-        free(temp);
+    else{               // Nếu stack không rỗng
+        temp = pTop;    // Gán giá trị temp với giá trị top 
+        pTop = pTop->prev;  // Cập nhật lại top là giá trị kề trước đó 
+        temp->prev = NULL;  // Cập nhật lại giá trị kề trước đó của temp là NULL
+        free(temp);        // Giải phóng bộ nhớ của temp
     }
 }
 
@@ -53,25 +61,25 @@ void pop(){
  * @brief Hàm stackEmpty() kiểm tra xem stack có rỗng hay không
  * 
  */
-int stackEmpty(){
-    return pTop == NULL;
+int stackEmpty(){       // Kiểm tra stack có rỗng hay không
+    return pTop == NULL;    // Nếu pTop = NULL thì stack rỗng 
 }
 
 /**
  * @brief Hàm printStack() in ra stack
  * 
  */
-void printStack(){
-    struct stackNode* temp;
-    if(pTop == NULL){
-        printf("\nStack is empty");
-        return;
+void printStack(){      // In ra stack 
+    struct stackNode* temp;  // Khai báo biến temp   
+    if(pTop == NULL){      // Nếu stack rỗng
+        printf("\nStack is empty");     // In ra thông báo
+        return;                    // Thoát khỏi hàm
     }
-    else{
-        temp = pTop;
-        while(temp != NULL){
-            printf("%d ", temp -> data);
-            temp = temp -> prev;
+    else{   // Nếu stack không rỗng
+        temp = pTop;       // Gán giá trị temp với giá trị top
+        while(temp != NULL){    // Lặp đến khi temp = NULL
+            printf("%d ", temp -> data);    // In ra giá trị của temp
+            temp = temp -> prev; // Cập nhật lại temp là giá trị kề trước đó
         }
     }
 }
